42cfca5b21cf96cfbabf5872bb4307f2b84ed191
Pritesh Shinde <priteshs@prdxn.com>
2019-07-16 00:44:08 +0530

    Sequelize ORM One-To-Many relationship established

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ require('dotenv').config();
 const error404Controller = require('./src/controllers/error404Controller');
 const routes = require('./src/routes/routes');
 const sequelize = require('./src/config/dbconfig2');
+const Product = require('./src/models/ProductSequelize');
+const User = require('./src/models/UserSequelize');
 
 // Best practices app settings
 app.set('title', 'My 1st App');
@@ -47,10 +49,14 @@ app.use(function(err, req, res, next) {
 });
 /* eslint-enable no-unused-vars */
 
+// One-To-One relationship
+Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'})
+User.hasMany(Product)
+
 // creates tables using models which are defined using sequelize into database if they doesn't exist
 sequelize.sync()
   .then(result => {
-    app.listen(app.get('port'), () => {
+    app.listen(process.env.APP_PORT, () => {
       console.log(`Find the server at: http://localhost:${process.env.APP_PORT}/`); // eslint-disable-line no-console
     });
   })
