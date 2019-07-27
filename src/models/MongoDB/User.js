a2b07a87218633a77a38bf83d1b4d606f3fdeda5
Pritesh Shinde <priteshs@prdxn.com>
2019-07-27 16:40:44 +0530

    MongoDB model modified

--- src/models/MongoDB/User.js
+++ src/models/MongoDB/User.js
@@ -3,12 +3,6 @@ const mongodb = require('mongodb')
 const getDB = require('../../config/dbconfig/MongoDB').getDB
 
 module.exports = class User {
-  constructor(name, description, price) {
-    this.name = name
-    this.description = description
-    this.price = price
-  }
-
   getAll() {
     const db = getDB()
     return db.collection('users').find().toArray()
@@ -22,16 +16,19 @@ module.exports = class User {
 
   save(user) {
     const db = getDB()
-    return db.collection('users').insertOne(user).then(result => result).catch(err => console.log(err))
+    // return db.collection('users').insertOne(user).then(result => result).catch(err => console.log(err))
+    return db.collection('users').insertOne(user)
   }
 
   update(user, id) {
     const db = getDB()
-    return db.collection('users').updateOne({_id: new mongodb.ObjectId(id)}, {$set: user}).then(result => result).catch(err => console.log(err))
+    // return db.collection('users').updateOne({_id: new mongodb.ObjectId(id)}, {$set: user}).then(result => result).catch(err => console.log(err))
+    return db.collection('users').updateOne({_id: new mongodb.ObjectId(id)}, {$set: user})
   }
 
   delete(id) {
     const db = getDB()
-    return db.collection('users').deleteOne({_id: new mongodb.ObjectId(id)}).then(result => result).catch(err => console.log(err))
+    // return db.collection('users').deleteOne({_id: new mongodb.ObjectId(id)}).then(result => result).catch(err => console.log(err))
+    return db.collection('users').deleteOne({_id: new mongodb.ObjectId(id)})
   }
 }
