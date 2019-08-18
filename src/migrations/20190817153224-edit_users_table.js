c31958aebe5ad954f57a6c17bb25e33ffb331bf5
Pritesh Shinde <priteshs@prdxn.com>
2019-08-18 22:16:20 +0530

    Modified column values

--- src/migrations/20190817153224-edit_users_table.js
+++ src/migrations/20190817153224-edit_users_table.js
@@ -8,6 +8,7 @@ module.exports = {
         'api_token',
         {
           type: Sequelize.STRING,
+          allowNull: false,
           defaultValue: '1234',
           after: 'password'
         }
@@ -17,6 +18,7 @@ module.exports = {
         'api_token_created_at',
         {
           type: Sequelize.DATE,
+          allowNull: false,
           defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
           after: 'api_token'
         }
