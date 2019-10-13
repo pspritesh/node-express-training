2a4fdb441bc9afa14e2103eaedeb5c3507447ac7
Pritesh Shinde <priteshs@prdxn.com>
2019-10-13 23:03:38 +0530

    Code optimised for CORS request

--- app.js
+++ app.js
@@ -27,10 +27,11 @@ app.set('views', path.join(__dirname, './src/views'))
 // Define templating engine for app
 app.set("view engine", "pug")
 
+// Handling CORS requests
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", 'clientUrl')
+  res.header("Access-Control-Allow-Origin", process.env.CLIENT_URL)
   res.header("Access-Control-Allow-Headers", "Origin, Authorization, X-Requested-With, Content-Type, Accept")
-  res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, HEAD, PUT, DELETE")
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS, HEAD")
   if ('OPTIONS' === req.method) {
     res.sendStatus(200)
   } else {
