8b69caac00c44ae29890e5804270ad6ded75bd3e
Pritesh Shinde <priteshs@prdxn.com>
2019-10-21 15:23:25 +0530

    Code optimised

--- test/start.js
+++ test/start.js
@@ -1,11 +1,11 @@
 const { expect } = require('chai')
 
-it('Should add numbers correctly', () => {
+it('Should add numbers correctly.', () => {
   const num1 = 2, num2 = 3
   expect(num1 + num2).to.equal(5)
 })
 
-it('Should not return 6', () => {
+it('Should not return 6.', () => {
   const num1 = 2, num2 = 3
   expect(num1 + num2).not.to.equal(6)
 })
