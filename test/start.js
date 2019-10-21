dc019eefb68806b8e7b0e21d9a4641b829071dae
Pritesh Shinde <priteshs@prdxn.com>
2019-10-21 11:45:07 +0530

    Implemented Unit Testing using mocha and chai

---
+++ test/start.js
@@ -0,0 +1,11 @@
+const expect = require('chai').expect
+
+it('Should add numbers correctly', () => {
+  const num1 = 2, num2 = 3
+  expect(num1 + num2).to.equal(5)
+})
+
+it('Should not return 6', () => {
+  const num1 = 2, num2 = 3
+  expect(num1 + num2).not.to.equal(6)
+})
