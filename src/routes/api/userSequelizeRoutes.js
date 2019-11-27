257fac7948158a733518415d577e321e68b8f4fa
Pritesh Shinde <priteshs@prdxn.com>
2019-11-27 21:46:16 +0530

    Code optimised

--- src/routes/api/userSequelizeRoutes.js
+++ src/routes/api/userSequelizeRoutes.js
@@ -6,25 +6,31 @@ const userSequelizeController = require('../../controllers/userSequelizeControll
 router.route('/products')
   .get(userSequelizeController.getAllProducts)
   .post(userSequelizeController.createProduct)
+
 router.route('/products/:productId')
   .put(userSequelizeController.updateProduct)
   .delete(userSequelizeController.deleteProduct)
 
+
 router.route('/products/:userId/mtm')
   .get(userSequelizeController.getProduct)
   .post(userSequelizeController.addNewProduct)
+
 router.route('/products/:productId/image')
   .get(userSequelizeController.getProductImage)
   .post(userSequelizeController.addNewProductImage)
 router.post('/products/:userId/:productId', userSequelizeController.assignProduct)
 
+
 router.route('/')
   .get(userSequelizeController.getUsers)
   .post(userSequelizeController.addUser)
+
 router.route('/:userId')
   .get(userSequelizeController.getUser)
   .put(userSequelizeController.updateUser)
   .delete(userSequelizeController.deleteUser)
+
 
 router.get('/:productId/pdf', userSequelizeController.generatePDF)
 
